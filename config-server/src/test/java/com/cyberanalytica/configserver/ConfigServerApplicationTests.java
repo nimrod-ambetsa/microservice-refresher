3746eefef559232d60237ff6774f122697c293f2
Paddy-Nimrod <n.ambetsa21@gmail.com>
2025-06-21 16:29:18 +0300

    first commit

---
+++ config-server/src/test/java/com/cyberanalytica/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberanalytica.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
